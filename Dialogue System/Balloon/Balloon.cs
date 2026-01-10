23802fe420d003fca6893a7c9ebf819974c9ecfa
ElNeonnn <jndrsn.08@gmail.com>
2026-01-10 15:12:09 -0300

    implementei a função _Ready para atribuir as variáveis speaker e line com a linha atual e seu locutor

--- Dialogue System/Balloon/Balloon.cs
+++ Dialogue System/Balloon/Balloon.cs
@@ -3,8 +3,20 @@ using System;
 
 public partial class Balloon : Control
 {
+	[Export] private Label SpeakerLabel { get; set; }
+	[Export] private RichTextLabel LineLabel { get; set; }
+
 	private DialogueScene _dialogue;
 	private int index;
+
+	private DialogueSpeaker speaker;
+	private DialogueLine line;
+
+    public override void _Ready()
+    {
+		speaker = _dialogue.Lines[index].Speaker;
+		line = _dialogue.Lines[index];
+    }
 
 	public void StartDialogue(DialogueScene dialogue)
 	{
