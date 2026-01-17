51dc7271239b6c769d82369cf1376ab71c06c1a7
ElNeonnn <jndrsn.08@gmail.com>
2026-01-17 17:53:43 -0300

    criei o recurso DialogueOption que representa uma opção

--- Dialogue System/Resources/DialogueLine.cs
+++ Dialogue System/Resources/DialogueLine.cs
@@ -6,4 +6,5 @@ public partial class DialogueLine : Resource
 {
     [Export] public DialogueSpeaker Speaker { get; set; }
     [Export(PropertyHint.MultilineText)] public string Text { get; set; }
+    [Export] public DialogueOption[] Options { get; set; }
 }
